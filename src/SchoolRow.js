350743ae0d4e479c10fab10e60462ff860239909
Andre Amaral <amaralandre92@gmail.com>
2019-05-22 18:22:01 -0300

    added to SchoolRow

--- src/SchoolRow.js
+++ src/SchoolRow.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 
 class SchoolRow extends Component{
+    schoolStats() {
+        //need routing to a page with all the school stats. how?
+        //https://www.youtube.com/watch?v=bqSSLr8A8PU&list=PL1J2GubvF0gBkbM3R6PaQu8OY42U9flTI&index=6&t=2353s
+        const url = "http://www.google.com"
+        window.location.href = url
+    }
+    
     render(){
         return <div key={this.props.school.id}>
                 <h4>
@@ -9,6 +16,7 @@ class SchoolRow extends Component{
                 <p>
                     {this.props.school.owner}
                 </p>
+                <input type="button" onClick={this.schoolStats.bind(this)} value="Estatisticas"></input>
       </div>
     }
 }
