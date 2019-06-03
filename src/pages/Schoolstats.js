43f143e9c27435e3de91ccaeda60bf6c2661175e
Andre Amaral <amaralandre92@gmail.com>
2019-06-03 17:49:33 -0300

    added roadmap

--- src/pages/Schoolstats.js
+++ src/pages/Schoolstats.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 //import api from '../../api/api';
+import {Bar} from 'react-chartjs-2';
 
 export default class Schoolstats extends Component{
     state = {
@@ -14,7 +15,6 @@ export default class Schoolstats extends Component{
         const response = await fetch(url);
         const data = await response.json();
         this.setState({ school: data.results[0], loading: false });
-        //console.log(data.result[0])
     }
 
     render(){
@@ -23,6 +23,7 @@ export default class Schoolstats extends Component{
                 {this.state.loading || !this.state.school ? ( <div>Carregando</div>) : 
                 (
                     <div>
+                        <h1>{this.state.school.year}</h1>
                         <h3>{this.state.school.school_name}</h3>
                         <h4>{this.state.school.city} - {this.state.school.state}</h4>
                         <p>{this.state.school.type}</p>
@@ -37,6 +38,19 @@ export default class Schoolstats extends Component{
         );
     }
 }
+
+//OBJETIVOS:
+// 1. GRAFICO COM MEDIAS DO ANO SELECIONADO
+// 2. GRAFICO COM HISTORICO DE MEDIAS
+// 3. INCLUIR MEDIA MUNICIPAL, ESTADUAL E NACIONAL
+// 4. SECAO DE DADOS SOCIAIS: % DE RACA E GENERO DA ESCOLA
+
+// PERGUNTAS A SEREM RESPONDIDAS:
+//1. DIFERENCA DE MEDIA ENTRE OS ANOS
+//2. QUAIS SAO OS ESTADOS QUE TEM OS MELHORES ALUNOS E OS PIORES
+//3. HA DIFERENCAS ENTRE MEDIA DE RACAS OU GENEROS?
+//4. MEDIA NACIONAL E DO ESTADO COMPARADO COM A ESCOLA
+//5. MEDIA ESCOLA PUBLICA VERSUS ESCOLA PRIVADA
 
 // <div key={this.props.school.id}>
 //                 <h4>{this.props.school.name}, {this.props.school.state}</h4>
